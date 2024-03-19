725f7252ba1fe7eb0c2d13113c3be1bae7450318
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-20 01:18:54 +0530

    initialize chat system

--- chat/chat.go
+++ chat/chat.go
@@ -1,1 +1,20 @@
 package chat
+
+import "github.com/Rishi-Mishra0704/code-collab-backend/network"
+
+// ChatMessage represents a message in the chat system.
+type ChatMessage struct {
+	network.Peer
+	Content   string
+	Timestamp string
+}
+type ChatService struct {
+	network.Transport // Embed the network.Transport interface
+}
+
+// NewChatService creates a new instance of ChatService.
+func NewChatService(transport network.Transport) *ChatService {
+	return &ChatService{
+		Transport: transport,
+	}
+}

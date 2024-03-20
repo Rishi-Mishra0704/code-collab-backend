d66385c499242afb755d2a2018459ff6c1858303
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-21 02:03:07 +0530

    add chat controller and implement api

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/Rishi-Mishra0704/code-collab-backend/controllers"
+	"github.com/Rishi-Mishra0704/code-collab-backend/network"
+)
 
 func main() {
-	fmt.Print("Hello")
+	// Initialize TCP transport
+	transport := network.NewTCPTransport()
+
+	// Initialize ChatController with ChatServiceport
+	chatController := controllers.NewChatController(transport)
+
+	// Initialize Gin router
+	router := gin.Default()
+
+	// Define API endpoints using the ChatController methods
+	router.POST("/create-room", chatController.CreateRoom)
+	router.POST("/join-room/:roomID", chatController.JoinRoom)
+	router.POST("/leave-room/:roomID/:peerID", chatController.LeaveRoom)
+
+	// Start Gin server
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }

936c44f82eba8f9b2457f343df06a2b23ff2faac
Sarvesh-shinde23 <shindesarvesh023@gmail.com>
2024-03-20 20:29:50 +0530

    add login and signup

--- auth/signup.go
+++ auth/signup.go
@@ -1,1 +1,56 @@
 package auth
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var users = map[string]User{}
+
+func SignupHandler(c *gin.Context) {
+	var newUser User
+	if err := c.BindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+		return
+	}
+	if _, exists := users[newUser.Username]; exists {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Username already exists"})
+		return
+	}
+	for _, existingUser := range users {
+		if existingUser.Email == newUser.Email {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Email already exists"})
+			return
+		}
+	}
+
+	users[newUser.Username] = newUser
+	c.JSON(http.StatusOK, gin.H{"message": "User signed up successfully"})
+}
+
+func LoginHandler(c *gin.Context) {
+	var loginRequest struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	if err := c.BindJSON(&loginRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid request payload"})
+		return
+	}
+	user, exists := users[loginRequest.Username]
+	if !exists || user.Password != loginRequest.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User logged in successfully"})
+
+}
